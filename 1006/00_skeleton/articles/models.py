a558399781a2bed11e9e30b7af0da67bb1b0b6e7
subin3277 <subin3277@gmail.com>
2022-10-06 13:58:48 +0900

    db

---
+++ 1006/00_skeleton/articles/models.py
@@ -0,0 +1,25 @@
+from operator import mod
+from statistics import mode
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comment')
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.content
